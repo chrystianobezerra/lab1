46cb2d07baa4da8dfd2e2fa85b05f9856916b794
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-06 20:19:21 -0300

    Teste de acesso a um fortigate com sucesso

--- ssh-access-template.py
+++ ssh-access-template.py
@@ -1,6 +1,7 @@
 #Script para acesso SSH
 
 from netmiko import ConnectHandler
+import json
 
 Device1 = {
     'device_type': 'cisco_ios',
@@ -9,14 +10,19 @@ Device1 = {
     'password': input("Digite a senha:"),
 }
 
-menu = int(input("Digite: "
-                    "<1> Para conectar ou manter conectado: "
-                    "<2> Para desconectar: "))
-
-while menu>0 and menu<3:
-    if menu==1:
-        net_connect = ConnectHandler(**Device1)
-        output = net_connect.send_command(input("Digite o comando: "))
-        print(output)
-    elif menu==2:
-        break
+#menu = int(input("Digite: "
+#                    "<1> Para conectar ou manter conectado: "
+#                    "<2> Para desconectar: "))
+#
+#while menu>0 and menu<3:
+#    if menu==1:
+#        net_connect = ConnectHandler(**Device1)
+#        output = net_connect.send_command(input("Digite o comando: "))
+#        print(output)
+#    elif menu==2:
+#        break
+net_connect = ConnectHandler(**Device1)
+output = net_connect.send_command('diag ip address list')
+#print(output)
+with open("J:\Device1.txt", "w") as arquivo:
+    arquivo.write(output)
