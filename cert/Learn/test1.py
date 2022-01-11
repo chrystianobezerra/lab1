a87969b880e1c64697ba6e3bd5baa567df83d594
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-11 19:32:24 -0300

    Commit para inicio

---
+++ cert/Learn/test1.py
@@ -0,0 +1,18 @@
+#primeiro_nome = input("Digite seu nome: ").upper()
+#sobre_nome = input("Digite seu sobre nome: ").upper()
+#print(primeiro_nome +"\r"+ sobre_nome)
+
+#n = int(input("Digite um numero de 0 a 100: "))
+#if n >= 30:
+#    print('The number is 20')
+
+
+#for x in range (3):
+#    print (x)
+
+count = 1
+while (count < 5):
+    print("Loop count is:", count)
+    count = count + 1
+else:
+    print("loop is finished")
