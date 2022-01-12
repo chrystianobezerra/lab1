6cc7fc4c7af0a33f936414b156eae82d8c0a8aa0
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-12 18:58:04 -0300

    Aprendizado de manipulação de arquivos cap5

--- cert/Learn/files-script.py
+++ cert/Learn/files-script.py
@@ -1,4 +1,13 @@
 # Scripts de lab para manipulação de arquivos cap5
 
-readdata = open("ctb.txt", "r")
-print(readdata.read())
+#Primeira forma de ler o arquivo
+#readdata = open("ctb.txt", "r")
+#print(readdata.read())
+#readdata.close()
+
+#Segunda forma de ler o arquivo
+with open("ctb.txt", "a+") as data:
+    data.write(input("Digite o que deseja inserir no arquivo: "'\n'))
+
+with open("ctb.txt", "r") as data:
+    print(data.read())
