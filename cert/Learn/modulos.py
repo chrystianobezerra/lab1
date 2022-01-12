fb3139860af8e14e9f3c043c9b81be5f2a0a70dd
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-11 21:47:21 -0300

    Aprendizado de classes e funções cap 4

--- cert/Learn/modulos.py
+++ cert/Learn/modulos.py
@@ -1,6 +1,24 @@
 #Este arquivo possui todas as funções e modulos utilizados no laboratório
+class Router:
+    '''Router Class'''
+    def __init__(self, model, swversion, ip_add):
+        '''inicialize values'''
+        self.model = model
+        self.swversion = swversion
+        self.ip_add = ip_add
 
+    def getdesc(self):
+        '''return a formatted description of the router'''
+        desc = f'Router Model               :{self.model}\n'\
+               f'Software Version           :{self.swversion}\n'\
+               f'Router Management Address  :{self.ip_add}'
+        return desc
 
-def devnet():
-    #Função para teste inicial
-    print("Teste DevNet")
+class Switch (Router):
+    def getdesc(self):
+        '''return a formatted description of the switch'''
+
+        desc = (f'Switch Model              :{self.model}\n'
+                f'Software Version          :{self.swversion}\n'
+                f'Switch Management Address :{self.ip_add}')
+        return desc
