067ab3b13996fe9f82381517482561064cde857f
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-12 22:13:58 -0300

    Aprendizado de manipulação de arquivos cap5

--- cert/Learn/unitest1.py
+++ cert/Learn/unitest1.py
@@ -1,11 +1,12 @@
 #Script the testunit
 
 import unittest
+from unittest import TestCase
 
 from test_area_of_circle import area_of_circle
 from math import pi
 
-class Test_Area_of_Circle_input (unittest,TestCase):
+class Test_Area_of_Circle_input (unittest.TestCase):
     def test_area(self):
         #Test radius >= 0
         self.assertAlmostEqual(area_of_circle(1), pi)
