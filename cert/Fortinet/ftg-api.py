bcdf368de9d03fb1b8d1b206d66ad776545874a0
Chrystiano Tamarozzi Bezerra <chrystiano.bezerra@advanta.com.br>
2022-01-16 19:46:36 -0300

    CAP7 teste de função requests python

--- cert/Fortinet/ftg-api.py
+++ cert/Fortinet/ftg-api.py
@@ -1,3 +1,13 @@
 #Teste de integração da api para fortigate
 
-import fortigate-api
+from netmiko import Netmiko
+
+fgt = {'host':'192.168.145.2',
+       'username':'admin',
+       'password':'D@m0v0123',
+       'device_type':'fortinet'}
+
+print(f"{'#'*20} Conectando ao Dispositivo {'#'*20}")
+net_connect = Netmiko(**fgt)
+print(net_connect.find_prompt())
+print(f"{'#'*20} Conectado {'#'*20}")
